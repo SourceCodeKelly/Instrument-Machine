b579b75ae496285473a7762ab341ecf29608c185
Anthony <96215255+SourceCodeKelly@users.noreply.github.com>
2022-04-13 00:48:54 -0400

    Successfully play sound

--- scripts/app.js
+++ scripts/app.js
@@ -19,47 +19,47 @@ document.addEventListener("keypress", function(event) {
 function sound(key) {
     switch (key) {
         case "a":
-            let sound1 = new Audio("");
+            let sound1 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3");
             sound1.play();
             break;
 
         case "s":
-            let sound2 = new Audio("");
+            let sound2 = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3");
             sound2.play();
             break;
 
         case "d":
-            let sound3 = new Audio('');
+            let sound3 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3');
             sound3.play();
             break;
 
         case "f":
-            let sound4 = new Audio('');
+            let sound4 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Give_us_a_light.mp3');
             sound4.play();
             break;
 
         case "g":
-            let sound5 = new Audio('');
+            let sound5 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Bld_H1.mp3');
             sound5.play();
             break;
 
         case "h":
-            let sound6 = new Audio('');
+            let sound6 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Brk_Snr.mp3');
             sound6.play();
             break;
 
         case "j":
-            let sound7 = new Audio('');
+            let sound7 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3');
             sound7.play();
             break;
         
         case "k":
-            let sound8 = new Audio('');
+            let sound8 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3');
             sound8.play();
             break;
         
         case "l":
-            let sound9 = new Audio('');
+            let sound9 = new Audio('https://s3.amazonaws.com/freecodecamp/drums/side_stick_1.mp3');
             sound9.play();
             break;
 
